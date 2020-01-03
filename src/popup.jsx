afd455360411ebd05b7a22e2c3b5ef595448be6e
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2020-01-03 17:17:00 +0300

    feat: create webpack browser extension

---
+++ src/popup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css";
+
+const element = <div>Hello World!</div>;
+ReactDOM.render(element, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+if (!process.env.NODE_ENV) {
+  // eslint-disable-next-line no-console
+  console.error("NODE_ENV environment variable has not been set.");
+  process.exit(1);
+}
+
+const config = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    popup: "./src/popup.jsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-[contenthash].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+            },
+          },
+          {
+            loader: "eslint-loader",
+            options: {
+              emitWarning: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "/",
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/*", "!.gitkeep"],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name]-[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin([{ from: "public", to: "" }]),
+    new FriendlyErrorsWebpackPlugin({}),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
+
+module.exports = config;
+
+if (process.env.NODE_ENV === "development") {
+  module.exports = {
+    ...config,
+    devtool: "cheap-source-map",
+    // Ref: https://github.com/geowarin/friendly-errors-webpack-plugin#turn-off-errors
+    devServer: {
+      quiet: true,
+    },
+  };
+}
