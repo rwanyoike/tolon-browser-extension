1aad2b08c2c1b677c764c8ce28bc0189a59a8a6f
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2024-10-07 13:32:28 +0300

    chore: create zip on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import zipPack from "vite-plugin-zip-pack";
+import { name, version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react()],
+  plugins: [
+    vanillaExtractPlugin(),
+    react(),
+    zipPack({
+      outFileName: `${name}_v${version}.zip`,
+    }),
+  ],
   test: {
     environment: "happy-dom",
     setupFiles: ["./vitest.setup.ts"],
